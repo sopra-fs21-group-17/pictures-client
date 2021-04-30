55f33cba190414d2839f8e19ad8f876dc1a7579d
lakicv <71370438+lakicv@users.noreply.github.com>
2021-04-30 14:28:07 +0200

    Update AppRouter.js

--- src/components/shared/routers/AppRouter.js
+++ src/components/shared/routers/AppRouter.js
@@ -16,6 +16,11 @@ import BuildScreen from "../../game/BuildScreen";
 import ScoreScreen from "../../game/ScoreScreen";
 import MainBoard from "../../game/MainBoard";
 import {SetTemplate} from "../../Sets/SetTemplate";
+import Lobby from "../../game/Lobby";
+import ScoreScreen from "../../game/ScoreScreen";
+import GuessingScreen from "../../game/GuessingScreen";
+import BuildScreen from "../../game/BuildScreen";
+import MainBoard from "../../game/MainBoard";
 
 
 
