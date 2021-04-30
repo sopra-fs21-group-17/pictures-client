3a6bc1c0522111e3db5b802aeccc8bf5d6c34b00
Julia <julia.kostadinova@hotmail.com>
2021-04-30 22:36:00 +0200

    Change in Pictures

--- src/components/game/PictureGrid.js
+++ src/components/game/PictureGrid.js
@@ -70,7 +70,7 @@ class PictureGrid extends React.Component{
         super(props);
         this.state ={
             currentGameState: "start",  /*idea to allow picture selection when in state guessing for the future*/
-            pictureURLs: null,
+            pictureURLs: [],
             coordinate: [0,1,2,3,
                 4,5,6,7,
                 8,9,10,11,
@@ -89,6 +89,7 @@ class PictureGrid extends React.Component{
             alert(`Something went wrong getting the Pictures: \n${handleError(error)}`);
         }
     }
+componentDidMount() {}
 
     render(){
 
