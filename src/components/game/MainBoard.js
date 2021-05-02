d4f694a4109940028b3298cf0d9293716f17a6c8
oliver <seriously251@gmail.com>
2021-05-02 03:25:30 +0200

    picture grid for demo

--- src/components/game/MainBoard.js
+++ src/components/game/MainBoard.js
@@ -114,6 +114,24 @@ class MainBoard extends React.Component{
                 "B1", "B2", "B3", "B4",
                 "C1", "C2", "C3", "C4",
                 "D1", "D2", "D3", "D4"],
+            picsURLs: [
+                "https://homepages.cae.wisc.edu/~ece533/images/peppers.png", //1
+                "https://homepages.cae.wisc.edu/~ece533/images/monarch.png", //2
+                "https://homepages.cae.wisc.edu/~ece533/images/pool.png", //3
+                "https://homepages.cae.wisc.edu/~ece533/images/tulips.png", //4
+                "https://homepages.cae.wisc.edu/~ece533/images/watch.png", //5
+                "https://homepages.cae.wisc.edu/~ece533/images/sails.bmp", //6
+                "https://homepages.cae.wisc.edu/~ece533/images/fruits.png", //7
+                "https://homepages.cae.wisc.edu/~ece533/images/cat.png", //8
+                "https://homepages.cae.wisc.edu/~ece533/images/boy.bmp", //9
+                "https://picsum.photos/id/237/200/300",//10
+                "https://www.diabete.qc.ca/wp-content/uploads/2014/08/Les-fruits.png",//11
+                "https://homepages.cae.wisc.edu/~ece533/images/baboon.png",//12
+                "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-168504892-1568303467.png?crop=1.00xw:0.606xh;0,0.0671xh&resize=980:*",//13
+                "https://images.unsplash.com/photo-1486578077620-8a022ddd481f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80",//14
+                "https://assets.eflorist.com/site/EF-3626/Homepage/Spring_Flowers.PNG",//15
+                "https://hgtvhome.sndimg.com/content/dam/images/hgtv/stock/2018/3/2/shutterstock_anemone-134595248.jpg.rend.hgtvcom.966.644.suffix/1519931799331.jpeg"//16
+            ],
             pictureURLs: null,
             coordinate: [0,1,2,3,
                 4,5,6,7,
@@ -124,12 +142,11 @@ class MainBoard extends React.Component{
     }
 
     //API REQUESTS//
-// TODO delete this comment
+
     async getPlayersFromLobby(){
         try {
             const response = await api.get("/players");
             this.setState({players: response});
-
         }
         catch (error) {
             alert(`Something went wrong getting the Players: \n${handleError(error)}`);
@@ -154,6 +171,7 @@ class MainBoard extends React.Component{
                         mySet: val.assignedSet,
                         myCoordinates: this.state.coordinateNames[val.assignedCoordinates]
                     });
+                    localStorage.setItem("myPicURL", this.state.picsURLs[val.assignedCoordinates])
                     break;
                 }
             }
