ea5e181f065db30221c9d53a9284acb234bf4c48
dkajin <dominik.kajinic@uzh.ch>
2021-05-15 11:48:21 +0200

    add scrollfunction + some changes

--- src/components/game/BuildScreen.js
+++ src/components/game/BuildScreen.js
@@ -13,10 +13,10 @@ const Container = styled(BaseContainer)`
   text-align: center;
 `;
 
-const Countdown = styled.h1`
+const Countdown = styled.div`
   margin-top = 10px;
   margin-bottom = 20px;
-  font-size = 100px;
+  
 `;
 
 class BuildScreen extends React.Component {
@@ -92,7 +92,8 @@ class BuildScreen extends React.Component {
         return (
             <Container>
                 {this.state.countMin <= 0 && (this.state.count + buildRoom.timeDifference) <= 0 ? (this.userFinishedBuilding()):(
-                <Countdown>Countdown: {this.state.countMin}:{Math.round(this.state.count + buildRoom.timeDifference)}</Countdown>)}
+                    <Countdown style={{fontSize: "50px"}}>Time left:<br></br>
+                    <span style={{fontWeight: "bold", fontFamily: "\"Open Sans\", sans-serif;", color: "white"}}>{this.state.countMin}:{Math.round(this.state.count + buildRoom.timeDifference)}</span></Countdown>)}
 
                 <div>
                     <SetTemplate pictureURL={localStorage.getItem("myPicURL")}/>
